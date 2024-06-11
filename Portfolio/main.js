858c263adebf7245f6a8e5d2843500e8b5f50489
Techie <ramyasrigarlapati28@gmail.com>
2024-06-11 15:44:31 +0530

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,8 @@
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Ramya Garlapati."],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
